d7bad32ad7c6108861018fe9d6e1a960f81288b6
desmo <62178867+desmodrone@users.noreply.github.com>
2023-09-06 17:32:42 +0200

    rustlings homework

--- homeworks_rust/homeworks/homework6/collections/hashmap2.rs
+++ homeworks_rust/homeworks/homework6/collections/hashmap2.rs
@@ -12,8 +12,6 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -23,6 +21,8 @@ enum Fruit {
     Mango,
     Lychee,
     Pineapple,
+    Strawberry,
+    Pear,
 }
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
@@ -32,12 +32,18 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Mango,
         Fruit::Lychee,
         Fruit::Pineapple,
+        Fruit::Strawberry,
+        Fruit::Pear,
     ];
 
     for fruit in fruit_kinds {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        basket.insert(Fruit::Pineapple, 2);
+        basket.insert(Fruit::Banana, 4);
+        basket.insert(Fruit::Strawberry, 7);
+        basket.insert(Fruit::Pear, 9);
     }
 }
 
--- homeworks_rust/homeworks/homework6/move_semantics/move_semantics1.rs
+++ homeworks_rust/homeworks/homework6/move_semantics/move_semantics1.rs
@@ -1,12 +1,12 @@
 // move_semantics1.rs
 // Make me compile! Execute `rustlings hint move_semantics1` for hints :)
 
-// I AM NOT DONE
+
 
 fn main() {
     let vec0 = Vec::new();
 
-    let vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -24,3 +24,4 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
     vec
 }
+
